3f51c87556ecacfc29a15d58558dbc24d77e050e
Bertrand Bourion <bertrandbou@hotmail.fr>
2022-10-12 20:32:57 +0200

    test: should 2 be equal to 2, test failed

--- src/FizzBuzz.test.js
+++ src/FizzBuzz.test.js
@@ -3,3 +3,7 @@ import fizzBuzz from './fizzBuzz';
 test('should return 1 if number is 1', () => {
   expect(fizzBuzz(1)).toBe(1);
 });
+
+test('should return 2 if number is 2', () => {
+  expect(fizzBuzz(1)).toBe(2);
+});
