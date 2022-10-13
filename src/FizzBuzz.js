6da909e94ad4349a2f779b1fe41a07a1c865d9c2
Bertrand Bourion <bertrandbou@hotmail.fr>
2022-10-13 20:47:38 +0200

    test: write function, test passed.

--- src/FizzBuzz.js
+++ src/FizzBuzz.js
@@ -1,6 +1,7 @@
 import PropTypes from 'prop-types';
 
 const fizzBuzz = (nb) => {
+  if (nb % 15 === 0) return 'FizzBuzz';
   if (nb % 3 === 0) return 'Fizz';
   if (nb % 5 === 0) return 'Buzz';
   return nb;
