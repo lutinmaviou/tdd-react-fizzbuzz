4f42281bac7f69e5b3d9013be64b3bfffef1f4df
Bertrand Bourion <bertrandbou@hotmail.fr>
2022-10-13 21:04:58 +0200

    test: should passed numbers 1 and 2 return 12: test failed

--- src/FizzBuzz.test.js
+++ src/FizzBuzz.test.js
@@ -9,25 +9,29 @@ test('should return 2 if number is 2', () => {
 });
 
 test('should return Fizz if number is 3', () => {
-  expect(fizzBuzz(3)).toBe("Fizz");
+  expect(fizzBuzz(3)).toBe('Fizz');
 });
 
 test('should return Fizz if number is 6', () => {
-  expect(fizzBuzz(6)).toBe("Fizz");
+  expect(fizzBuzz(6)).toBe('Fizz');
 });
 
 test('should return Buzz if number is 5', () => {
-  expect(fizzBuzz(5)).toBe("Buzz");
+  expect(fizzBuzz(5)).toBe('Buzz');
 });
 
 test('should return Buzz if number is 10', () => {
-  expect(fizzBuzz(10)).toBe("Buzz");
+  expect(fizzBuzz(10)).toBe('Buzz');
 });
 
 test('should return FizzBuzz if number is 15', () => {
-  expect(fizzBuzz(15)).toBe("FizzBuzz");
+  expect(fizzBuzz(15)).toBe('FizzBuzz');
 });
 
 test('should return FizzBuzz if number is 30', () => {
-  expect(fizzBuzz(30)).toBe("FizzBuzz");
+  expect(fizzBuzz(30)).toBe('FizzBuzz');
+});
+
+test('should return 12 if numbers are 1 and 2', () => {
+  expect(fizzBuzz(1, 2)).toBe('12');
 });
