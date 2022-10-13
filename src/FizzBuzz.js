c6b6cf94e1de64f182f6d83b06eca8f160fb7747
Bertrand Bourion <bertrandbou@hotmail.fr>
2022-10-13 19:39:37 +0200

    feat: add prop types

--- src/FizzBuzz.js
+++ src/FizzBuzz.js
@@ -1,8 +1,11 @@
+import PropTypes from 'prop-types';
+
 const fizzBuzz = (nb) => {
-  if (nb === 3) {
-    return 'Fizz';
-  }
   return nb;
+};
+
+fizzBuzz.PropTypes = {
+  nb: PropTypes.number,
 };
 
 export default fizzBuzz;
